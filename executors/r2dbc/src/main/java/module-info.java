232e4e0e36962a45542571b00c737a375f261cb8
Cromefire_ <cromefire_@outlook.com>
2021-01-25 01:03:16 +0100

    Fixed build

--- executors/r2dbc/src/main/java/module-info.java
+++ executors/r2dbc/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module mikrate.executors.r2dbc {
     requires transitive mikrate.executors.api;
     requires r2dbc.spi;
-    requires kotlinx.coroutines.reactive;
+    //requires kotlinx.coroutines.reactive; // TODO: Comment in once https://github.com/Kotlin/kotlinx.coroutines/issues/2237 is fixed
 }
