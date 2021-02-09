d66ce5050bb5a9bee9fcebb06541e4861001c192
Cromefire_ <cromefire_@outlook.com>
2021-02-09 01:48:46 +0100

    More efficient container startup

--- dialects/postgres/src/test/kotlin/factory/mikrate/dialects/postgres/CreateTableTest.kt
+++ dialects/postgres/src/test/kotlin/factory/mikrate/dialects/postgres/CreateTableTest.kt
@@ -3,18 +3,18 @@ package factory.mikrate.dialects.postgres
 import factory.mikrate.core.types.IntegerType
 import factory.mikrate.dsl.migration
 import io.kotest.core.spec.style.ShouldSpec
-import io.kotest.extensions.testcontainers.perTest
+import io.kotest.extensions.testcontainers.perSpec
 import io.kotest.matchers.ints.shouldBeExactly
 
 class CreateTableTest : ShouldSpec({
     val postgresContainer = createPostgresContainer()
-    listener(postgresContainer.perTest())
+    listener(postgresContainer.perSpec())
 
     should("create a basic table") {
         val conn = postgresContainer.connect()
         val createTable by migration {
             up {
-                createTable("TestTable") {
+                createTable("TestTable1") {
                     column("id", IntegerType) {
                         primary()
                     }
@@ -29,7 +29,7 @@ class CreateTableTest : ShouldSpec({
 
         // Verify creation
         conn.createStatement().use {
-            it.executeUpdate("""INSERT INTO TestTable VALUES (1);""") shouldBeExactly 1
+            it.executeUpdate("""INSERT INTO TestTable1 VALUES (1);""") shouldBeExactly 1
         }
     }
 })
