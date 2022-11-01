2fa481de55d6aa8de1a1b93826ac7eb9dd7988c2
Cromefire_ <cromefire_@outlook.com>
2022-11-01 16:14:36 +0100

    Fixed CI build

--- core/src/main/kotlin/factory/mikrate/core/types/TextType.kt
+++ core/src/main/kotlin/factory/mikrate/core/types/TextType.kt
@@ -3,6 +3,6 @@ package factory.mikrate.core.types
 import factory.mikrate.core.DbType
 import factory.mikrate.dialects.api.models.DialectDbType
 
-public class TextType : DbType() {
+public object TextType : DbType() {
     override val dialectDbType: DialectDbType = DialectDbType.TextType
 }
