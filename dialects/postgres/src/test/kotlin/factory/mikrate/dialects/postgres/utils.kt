a435eeddbbe5b4b0004b274829df0fd6718daf73
Cromefire_ <cromefire+git@pm.me>
2023-10-13 01:05:08 +0200

    Added better waiting for postgres testcontainer

--- dialects/postgres/src/test/kotlin/factory/mikrate/dialects/postgres/utils.kt
+++ dialects/postgres/src/test/kotlin/factory/mikrate/dialects/postgres/utils.kt
@@ -2,19 +2,25 @@ package factory.mikrate.dialects.postgres
 
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy
 import org.testcontainers.lifecycle.Startable
 import java.sql.Connection
 import java.sql.DriverManager
+import java.time.Duration
 
 fun testContainer(image: String): GenericContainer<*> {
     return GenericContainer<Nothing>(image)
 }
 
 fun createPostgresContainer(): PostgresContainer = PostgresContainer(
-    testContainer("postgres:15-alpine")
+    testContainer("postgres:16-alpine")
         .withEnv("POSTGRES_PASSWORD", "postgres")
         .withExposedPorts(5432)
         .waitingFor(HostPortWaitStrategy())
+        .waitingFor(
+            LogMessageWaitStrategy().withRegEx(".*database system is ready to accept connections.*").withTimes(2)
+        )
+        .withStartupTimeout(Duration.ofSeconds(20))
 )
 
 class PostgresContainer(private val container: GenericContainer<*>) : Startable by container {
